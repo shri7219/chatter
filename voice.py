39454fb2c20b712d738eebfb70b0b4d706c068a7
m2dx2 <manishdixit18@gmail.com>
2023-07-08 20:36:21 +0530

    adding voice fixes

--- voice.py
+++ voice.py
@@ -1,17 +1,16 @@
 import requests
 
 CHUNK_SIZE = 1024
-url = "https://api.elevenlabs.io/v1/text-to-speech/DmbDXRnsDgyeMuXM9ZQN/stream"
+url = "https://api.elevenlabs.io/v1/text-to-speech/OkAVXWGnT75RtBZ4GbXS/stream"
 
 headers = {
     "Accept": "audio/mpeg",
     "Content-Type": "application/json",
-    "xi-api-key": "KEY"
+    "xi-api-key": "d060d080ed281f180151801279ba2552"
 }
 
 data = {
-    "text": "Welcome to ManipalCigna Health Insurance! Today, we have an interesting story to share with you. Meet Aari, a 35-year-old woman who recently encountered an unfortunate accident. Let's imagine how having ManipalCigna ProHealth Prime, our comprehensive health insurance plan, could have covered her medical expenses",
-
+    "text": "Once upon a time, in a bustling city, there lived a young woman named Ari. She was 25 years old, full of life, and had big dreams for her future. But one day, her world came crashing down when she was struck by excruciating pain throughout her body. Worried and desperate for relief, Ari rushed to the hospital.",
     "model_id": "eleven_monolingual_v1",
     "voice_settings": {
         "stability": 0.5,
